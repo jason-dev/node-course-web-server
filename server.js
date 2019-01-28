27c83e25c9eb5798520bb6ac17d9cfdafce0e184
Jason May <jmay1978@gmail.com>
2019-01-27 22:59:24 -0600

    Added projects page

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.use((req, res, next) => {
 });
 
 // app.use((req, res, next) => {
-
 //     res.render('maintenance.hbs');
-
 // });
 
 app.use(express.static(__dirname + '/public'));
@@ -55,6 +53,12 @@ app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
     })
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
 });
 
 app.listen(port, () => {
